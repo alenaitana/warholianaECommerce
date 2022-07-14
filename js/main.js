236a9c6723ff4a4c7a8c5b3e89a1b32e2636068d
alenaitana <alenaitana14@gmail.com>
2022-07-14 10:47:31 -0300

    subo todo los nuevos cambios al repositorio

--- js/main.js
+++ js/main.js
@@ -11,31 +11,57 @@ class producto {
         return this.precio;
     }
 }
-const producto1 = new producto("1", "totebags", "./assets/tiendalmg/corazonBlanco.jpeg", "varios colores", 2500);
-const producto2 = new producto("2", "prendedores","./assets/tiendalmg/prendedores.jpeg", "varios colores", 800);
-const producto3 = new producto("3", "totebags","./assets/tiendalmg/corazonNegro.jpeg", "varios colores", 3000);
-
-const productos = [];
-
-productos.push(producto1);
-productos.push(producto2);
-productos.push(producto3);  
-
+const productos = [ 
+    new producto("1", "totebags", "./corazonBlanco.jpeg", "varios colores", 2500),
+    new producto("2", "prendedores","./prendedores.jpeg", "varios colores", 800),
+    new producto("3", "totebags","./corazonNegro.jpeg", "varios colores", 3000)
+]
 function mostrarProductos(productos) {
-const contenedorDeProductos = document.getElementById("contenedorDeProductos");
-contenedorDeProductos.innerHTML = "";
+    const contenedorDeProductos = document.getElementById("contenedor-de-productos");
+    contenedorDeProductos.innerHTML = "";
 
-productos.forEach(producto => {
-    const divProducto = document.createElement("div");
-    divProducto.classList.add("producto")
-    divProducto.innerHTML = `
-        <img src="${producto.imagen}" alt="titulo"
-        <h2>${producto.nombre}<h2>
-        <p>color: ${producto.color} <p>
-        <p>Precio: ${producto.precio}
-    `;
+    productos.forEach(producto => {
+        const divProducto = document.createElement("div");
+            divProducto.classList.add("producto");
+            divProducto.innerHTML = `
+                <img height="400px" src="${producto.imagen}"<br>
+                <h2>${producto.nombre}</h2>
+                <p>color: ${producto.color} </p>
+                <p>Precio: ${producto.precio}</p>
+            `;
+            const botonAgregarAlCarrito = document.createElement("button")
+            botonAgregarAlCarrito.innerText = "Agregar al Carrito"
+            botonAgregarAlCarrito.addEventListener("click",() => {
+                agregarAlCarrito(producto);
+            })
 
-});
+        divProducto.appendChild(botonAgregarAlCarrito);
+
+        contenedorDeProductos.appendChild(divProducto);
+    })
 }
+
+function agregarAlCarrito(producto){
+    let carrito = JSON.parse(localStorage.getItem("carrito"))
+    if (carrito == null) {
+        carrito = []
+    }
+    carrito.push(producto);
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+document.getElementById("ver-carrito").addEventListener("click", verCarrito)
+
+function verCarrito(){
+    let carrito = JSON.parse(localStorage.getItem("carrito"))
+    if (carrito == null){
+        carrito = [];
+    }
+    mostrarProductos(carrito)
+}
+
+document.getElementById("ver-catalogo").addEventListener("click", ()=>{
+    mostrarProductos(productos);
+});
 
 mostrarProductos(productos);
